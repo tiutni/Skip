abb4d20b80eccfa1b2ecc34e035bc7cd0b78c844
garlic_dish <aksmfdyfl123@gmail.com>
2022-05-04 00:39:18 +0900

    2022/05/04 pay 관련 추가

--- src/main/java/com/skip/www/controller/PayController.java
+++ src/main/java/com/skip/www/controller/PayController.java
@@ -2,12 +2,48 @@ package com.skip.www.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.skip.www.service.face.PayService;
 
 @Controller
 public class PayController {
 
 	private static final Logger logger = LoggerFactory.getLogger(PayController.class);
 	
+	@Autowired PayService payService;
+	
+	@RequestMapping("/pay/concert")
+	public void selectExDate() {
+		
+	}
+	
+	@RequestMapping("/pay/exhibition")
+	public void selectConSeatAndDate() {
+		
+	}
+	
+	
+	@RequestMapping("/pay/confirm")
+	public void confirmBuyer() {
+		
+	}
+	
+	@RequestMapping("/pay/method")
+	public void selectPaymentMethod() {
+		
+	}
+	
+	@RequestMapping("/pay/consent")
+	public void getConsent() {
+		
+	}
+	
+	@RequestMapping("/refund/check")
+	public void checkRefund() {
+		
+	}
 	
 }
