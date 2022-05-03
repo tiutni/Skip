30ab5a916b828dd2e4b3b858a1784d539b8740f4
kmjnwk <kmjnwk@naver.com>
2022-05-03 15:09:42 +0900

    [2022.05.03 15:00]  Admin dto에 adminAuth 추가

--- src/main/java/com/skip/www/dto/Admin.java
+++ src/main/java/com/skip/www/dto/Admin.java
@@ -4,23 +4,35 @@ public class Admin {
 
 	private String adminId;
 	private String adminPw;
+	private String adminAuth;
 	
 	@Override
 	public String toString() {
 		return "Admin [adminId=" + adminId + ", adminPw=" + adminPw + "]";
 	}
-	
+
 	public String getAdminId() {
 		return adminId;
 	}
+
 	public void setAdminId(String adminId) {
 		this.adminId = adminId;
 	}
+	
+	public String getAdminAuth() {
+		return adminAuth;
+	}
+
+	public void setAdminAuth(String adminAuth) {
+		this.adminAuth = adminAuth;
+	}
+
 	public String getAdminPw() {
 		return adminPw;
 	}
+
 	public void setAdminPw(String adminPw) {
 		this.adminPw = adminPw;
 	}
-
+	
 }
