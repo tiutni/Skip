60e2b8b53bf9be7652dc10bd85e808e6e964f631
kmjnwk <kmjnwk@naver.com>
2022-05-17 18:01:09 +0900

    2022-05-17 17:58 김진욱 user interceptor 생성, servlet-context 수정

---
+++ src/main/java/com/skip/www/interceptor/UserInterceptor.java
@@ -0,0 +1,25 @@
+package com.skip.www.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class UserInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		HttpSession session = request.getSession();
+		if( null == session.getAttribute("login") ) { //비로그인 상태
+			response.sendRedirect("/user/login");
+			
+			return false;
+		}
+		
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+	
+}
