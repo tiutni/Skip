b84a50fe2d5775e1c87d69e6e3d3099179991ac6
jiyoung <wldud@gmail.com>
2022-05-20 16:36:12 +0900

    20220520 16:35 (지영) 결제(수정중), chatting.jsp 삭제

--- src/main/java/com/skip/www/controller/PayController.java
+++ src/main/java/com/skip/www/controller/PayController.java
@@ -1,12 +1,33 @@
 package com.skip.www.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
 import com.skip.www.dto.Concert;
 import com.skip.www.dto.Exhibition;
 import com.skip.www.dto.User;
@@ -19,9 +40,16 @@ public class PayController {
 	
 	@Autowired PayService payService;
 	
+	private IamportClient api;
+	
+	// 생성자 - IMPORT REST API 기본 설정
+	public PayController() {
+    	// REST API 키와 REST API secret 를 아래처럼 순서대로 입력한다.
+		this.api = new IamportClient("8367395191713875","fa7bd1b727d086ff1a5c93019047207f6115b0cb5323c53bb754f7a78ddf820e9591b6833ab607e3");
+	}
 	
 	
-	// 주문자정보 및 선택한 주문정보 조회
+	// 주문자정보 및 선택한 주문정보 조회 - 공연
 	@PostMapping("/pay/con")
 	public String selectedInfo(
 		int userNo				// 회원번호
@@ -64,7 +92,7 @@ public class PayController {
 		return "/pay/info";
 	}
 	
-	// 주문자정보 및 선택한 주문정보 조회
+	// 주문자정보 및 선택한 주문정보 조회 - 전시
 	@PostMapping("/pay/ex")
 	public String selectedInfo(
 			int userNo				// 회원번호
@@ -102,11 +130,25 @@ public class PayController {
 		return "/pay/info";
 	}
 	
+	
+	// IMPORT 결제 검증
+	@ResponseBody
+	@RequestMapping(value="/verifyIamport/{imp_uid}")
+	public IamportResponse<Payment> paymentByImpUid(
+			Model model
+			, Locale locale
+			, HttpSession session
+			, @PathVariable(value= "imp_uid") String imp_uid) throws IamportResponseException, IOException
+	{	
+			return api.paymentByImpUid(imp_uid);
+	}
+	
 	@PostMapping("/pay/complete")
 	public void successPay() {
 		
 		
 	}
+	
 	
 
 }
